356d8425223fd9d1ba5ff20dafa42572b5f31c2c
AJ ONeal <coolaj86@gmail.com>
2013-03-21 11:39:55 -0600

    removed legacy server directory

--- server/lib/index.js
+++
@@ -1,7 +0,0 @@
-/*jshint strict:true node:true es5:true onevar:true laxcomma:true laxbreak:true*/
-(function () {
-  "use strict";
-
-  module.exports.provider = require('./provider');
-  module.exports.consumer = require('./consumer');
-}());
